54c33ddf2e7ca3468f629b3f497092ae79f87a07
Johan <grimmerw968@gmail.com>
2022-02-14 22:29:53 +0200

    feat: add makrdown to html, css

--- meditation/src/components/Button.tsx
+++ meditation/src/components/Button.tsx
@@ -3,8 +3,17 @@ import './buttons.scss';
 
 export const Button: React.FC = () => (
     <div className="button__container">
-        <button className="button__item" aria-label="button timer" type="button" />
-        <button className="button__item" aria-label="button timer" type="button" />
-        <button className="button__item" aria-label="button timer" type="button" />
+        <button className="button__item" aria-label="button timer" type="button">
+            2 min
+        </button>
+        <button className="button__item" aria-label="button timer" type="button">
+            5 min
+        </button>
+        <button className="button__item" aria-label="button timer" type="button">
+            10 min
+        </button>
+        <button className="button__item" aria-label="button timer" type="button">
+            Infinity loop
+        </button>
     </div>
 );
--- meditation/src/components/ChangeBG.tsx
+++ meditation/src/components/ChangeBG.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import './changeBG.scss';
 
 export const ChangeBG: React.FC = () => (
-    <div>
-        <div>1</div>
+    <div className="bg__inner">
+        <button aria-label="buutton change BG" type="button" className="bg__btn__left">
+            left
+        </button>
+        <button aria-label="buutton change BG" type="button" className="bg__btn__rigth">
+            right
+        </button>
     </div>
 );
